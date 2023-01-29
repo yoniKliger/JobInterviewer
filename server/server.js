b8e3ca0fcfacc8ffb8c3e92d11a5fbeb7869afc8
Jonathan Carse <jonathan@vhive.ai>
2023-01-29 11:34:54 +0200

    changing port to 8080

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { routes } = require('./request/route');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const jsonParser = bodyParser.json()
 
