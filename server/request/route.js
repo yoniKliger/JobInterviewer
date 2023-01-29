cf0b8aecb90fd55f25b0468ff9d1563e1af4a8a1
Jonathan Carse <jonathan@vhive.ai>
2023-01-29 12:01:39 +0200

    added PUT route

--- server/request/route.js
+++ server/request/route.js
@@ -21,6 +21,25 @@ const postApplicationHandler = (req, res) => {
     res.json(newApplication);
 };
 
+const putApplicationHandler = (req, res) => {
+
+    const index = applications.findIndex(application => application.id === req.body.id);
+
+    if (index === -1) {
+        res.status(400);
+        res.send(`id ${req.body.id} not found`);
+    } else {
+        applications[index] = {
+            ...req.body,
+            createdAt: applications[index].createdAt,
+            updatedAt: new Date().toISOString()
+        }
+    
+        res.json(applications[index]);    
+    }
+
+};
+
 const routes = {
     get: [{
         path: '/application',
@@ -30,6 +49,11 @@ const routes = {
     post: [{
         path: '/application',
         handler: postApplicationHandler
+    }],
+
+    put: [{
+        path: '/application',
+        handler: putApplicationHandler
     }]
 }
 
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const jsonParser = bodyParser.json()
 
 routes.get.forEach(route => app.get(route.path, route.handler));
 routes.post.forEach(route => app.post(route.path, jsonParser, route.handler));
+routes.put.forEach(route => app.put(route.path, jsonParser, route.handler));
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`)
